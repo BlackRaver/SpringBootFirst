daf48b11ed85a8e0deaaa7d43fe1172e992aee34
BlackRaver <blackravern01@gmail.com>
2022-05-07 18:17:52 +0200

    Student class has been created

---
+++ src/main/java/student/Student.java
@@ -0,0 +1,11 @@
+package student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+}
